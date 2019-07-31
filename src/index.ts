76819f86700dc048803ff3ea944fc9f585c085c6
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2019-08-01 02:58:38 +0430

    index: sort of scripts were added

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,24 @@ import program from './shared/program'
 import { registerScripts } from './commands/scripts';
 
 registerScripts('yarn', [
-  [`t`, `Runs the package's test script`, `test`],
   [`s`, `Runs the package's start script`, `start`],
-  [`i`, `Installs dependencies`, `install`],
   [`sw`, `Runs the package's start script and watch`, `run start:watch`],
+  
+  [`t`, `Runs the package's test script`, `test`],
+
   [`b`, `Builds the package`, `run build`],
   [`bw`, `Builds and watches the package`, `run build:watch`],
+  
   [`v`, `Package version patch`, `version patch`],
   [`vm`, `Package version minor`, `version minor`],
   [`vM`, `Package version major`, `version major`],
-
-  [`r <script>`, `Installs dependencies`, `run <script>`],
-
+  
+  [`r <script>`, `Runs custom script`, `run <script>`],
+  
+  [`i`, `Installs dependencies`, `install`],
+  [`i <package>`, `Installs a dependency`, `add <package>`],
+  [`id <package>`, `Installs a devDependency`, `add -D <package>`],
+  [`ig <package>`, `Installs a global dependency`, `global add <package>`],
 ])
  
 program.parse(process.argv)
