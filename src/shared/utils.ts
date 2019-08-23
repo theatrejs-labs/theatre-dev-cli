b0bad1debd9078fe6fbb59eb3618becf81de28b6
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2019-08-23 23:37:41 +0430

    shared/utils: better error messages

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,8 +1,11 @@
 import execa from 'execa'
+import chalk from 'chalk'
 
 export const run = (command: string) => {
     const exec = execa.command(command)
-    exec.catch(console.error)
+    exec.catch(e => {
+        console.error(chalk`{bgRed.white  ERROR } ${e.command} {grey ${e.stderr}}`)
+    })
     if (exec.stdout) {
         exec.stdout.pipe(process.stdout);
     }
